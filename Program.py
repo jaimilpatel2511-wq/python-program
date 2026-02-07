e044346db3780a4bd9fa4b97675baa20dab0203e
jaimilpatel2511-wq <jaimilpatel2511@gmail.com>
2026-02-07 14:59:10 +0530

    Add files via upload

---
+++ Program.py
@@ -0,0 +1,18 @@
+a = int (input ("Enter value of a: "))
+b = int (input ("Enter value of b: "))
+
+if a > b :
+    print ("a is greater than b")
+elif b > a :
+    print ("b is greater than a")
+else :
+    print ("a and b are equal...")
+    
+if a >= 90 :
+    print ("Topper")
+elif a >= 70 :
+    print ("Good")
+elif a >= 50 :
+    print ("Average")
+else :
+    print ("Fail")
