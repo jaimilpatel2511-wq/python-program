f4bee3046a4acbfec73860a97e23c61727059c83
jaimilpatel2511-wq <jaimilpatel2511@gmail.com>
2026-02-08 22:48:37 +0530

    Add files via upload

---
+++ D.py
@@ -0,0 +1,20 @@
+
+for i in range (6) :
+    print (i)
+
+print ()
+
+for j in range (1,6) :
+    print (j)
+
+print ()
+    
+for k in range (3) :
+    print ("Hello")
+    print (k)
+    
+print ()
+    
+for l in range (1,3) :
+    print ("Hello")
+    print (l)
